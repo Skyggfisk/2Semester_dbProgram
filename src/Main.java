2b8846a6f75952eef263dadc76fa5def5a499234
Laurids Andersen <lauridsandersen2013@gmail.com>
2016-12-06 19:45:04 +0100

    here we go ...

--- src/Main.java
+++ src/Main.java
@@ -94,130 +94,6 @@ public class Main extends Application{
 		System.out.println("slaughteramount: " + AddValuesToDB.addValuesSlaughterAmount(slaughtervalue, batchnr, WorkingTeam.getInstance().getTeamTimeTableId(), time.getTime(), dbSinCon));
 	}
 	
-	/**
-	 * WARNING: adds 39.000 lines to a SQL file..
-	 */
-	private void fillSlaughterAmountTable() {
-		long daystart = 1480305600000L;
-		long oneday = 86400000L;
-		String tmpString = databasename;
-		BufferedWriter bf = createFile("SlaughterAmount");
-		AddValuesToDB.getCurrentTeamId(daystart, dbSinCon);
-		ArrayList<Integer> batch = AddValuesToDB.getBatchId(WorkingTeam.getInstance().getTeamTimeTableId(), dbSinCon);
-		int batchnr = batch.get(0);
-		int batchvalue = batch.get(1);
-		for (int i = 0; i < iterations; i++) {
-			int day = i%7;
-			if(day == 5 ||day == 6){
-				//tmpString += System.lineSeparator() + " sunday or saturday";
-			} else {
-				for (int j = 0; j < 480; j++) {
-					int slaughtervalue = rand.nextInt(16) + 200;
-					Long satimestamp = daystart + (j * 60000L);
-					AddValuesToDB.getCurrentTeamId(satimestamp, dbSinCon);
-					tmpString += System.lineSeparator() + "INSERT INTO slaughteramount (value, batchid, satimestamp, teamtimetableid) VALUES (" + slaughtervalue + ", " + batchnr + ", " + satimestamp + ", " + WorkingTeam.getInstance().getTeamTimeTableId() + ");";
-					batchvalue -= slaughtervalue;
-					if(batchvalue < 1){
-						batch = AddValuesToDB.getBatchId(WorkingTeam.getInstance().getTeamTimeTableId(), dbSinCon);
-						batchnr = batch.get(0);
-						batchvalue = batch.get(1);
-					}
-				}
-				addStringToFileBuffer(tmpString, bf);
-				tmpString = "";
-			}
-			daystart += oneday;
-		}
-		closeFile(bf);
-	}
-	
-	private BufferedWriter createFile(String filename){
-		FileWriter fw;
-		BufferedWriter bf = null;
-		try {
-			fw = new FileWriter(filename);
-			bf = new BufferedWriter(fw);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 
-		return bf;
-	}
-	
-	private void addStringToFileBuffer(String tmpString, BufferedWriter bf) {
-			try {
-				bf.write(tmpString);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-	}
-	
-	private void closeFile(BufferedWriter bf){
-		try {
-			bf.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * WARNING: creates 39.000 lines of SQL ...
-	 */
-	public void fillEmptyBraces(){
-		long daystart = 1480305600000L;
-		long oneday = 86400000L;
-		String tmpString = databasename;
-		BufferedWriter bf = createFile("EmptyBraces");
-		AddValuesToDB.getCurrentTeamId(daystart, dbSinCon);
-		for (int i = 0; i < iterations; i++) {
-			int day = i%7;
-			if(day == 5 ||day == 6){
-				//tmpString += System.lineSeparator() + " sunday or saturday";
-			} else {
-				for (int j = 0; j < 480; j++) {
-					Long starttimestamp = daystart + (j * 60000L);
-					int value = rand.nextInt(10);
-					AddValuesToDB.getCurrentTeamId(starttimestamp, dbSinCon);
-					tmpString += System.lineSeparator() + "INSERT INTO emptybraces (starttimestamp, value, teamtimetableid) VALUES (" + starttimestamp + ", " + value + ", " + WorkingTeam.getInstance().getTeamTimeTableId() + ");";
-					AddValuesToDB.getCurrentTeamId(starttimestamp, dbSinCon);
-				}
-				addStringToFileBuffer(tmpString, bf);
-				tmpString = "";
-			}
-			daystart += oneday;
-		}
-		closeFile(bf);
-	}
-	
-	public void fillSpeed(){
-		long daystart = 1480305600000L;
-		long oneday = 86400000L;
-		int targetval = 13000;
-		String tmpString = databasename;
-		BufferedWriter bf = createFile("Speed");
-		AddValuesToDB.getCurrentTeamId(daystart, dbSinCon);
-		for (int i = 0; i < iterations; i++) {
-			int day = i%7;
-			if(day == 5 ||day == 6){
-				//tmpString += System.lineSeparator() + " sunday or saturday";
-			} else {
-				for (int j = 0; j < 480; j++) {
-					Long stimestamp = daystart + (j * 60000L);
-					int speedval = rand.nextInt(5) + 13000;
-					if(AddValuesToDB.getOrganic(WorkingTeam.getInstance().getTeamTimeTableId(),dbSinCon)){
-						targetval = 6000;
-						speedval = rand.nextInt(5) + 6000;
-					}
-					AddValuesToDB.getCurrentTeamId(stimestamp, dbSinCon);
-					tmpString += System.lineSeparator() + "INSERT INTO speed (value, targetvalue, stimestamp) VALUES (" + speedval + ", " + targetval + ", " + stimestamp + ");";
-				}
-				addStringToFileBuffer(tmpString, bf);
-				tmpString = "";
-			}
-			daystart += oneday;
-		}
-		closeFile(bf);
-	}
-	
 	private static void fillBatches() {
 		long dayend = 1480334400000L;
 		long nightend = 1480305600000L;
@@ -445,6 +321,7 @@ public class Main extends Application{
 				//tmpString += System.lineSeparator() + " sunday or saturday";
 			} else {
 				for (int j = 0; j < 480; j++) {
+					System.out.println("prep: " + i);
 					Long satimestamp = daystart + (j * 60000L);
 					AddValuesToDB.getCurrentTeamId(satimestamp, dbSinCon);
 					teamidList.add(WorkingTeam.getInstance().getTeamTimeTableId());
--- src/SlaughterAmount.java
+++ src/SlaughterAmount.java
@@ -1,4 +1,3 @@
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
