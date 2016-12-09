4310f5c702db2644f9dbbd3513b9c76068249977
Laurids Andersen <lauridsandersen2013@gmail.com>
2016-12-09 10:59:10 +0100

    asd

--- src/SlaughterAmount.java
+++ src/SlaughterAmount.java
@@ -16,26 +16,30 @@ public class SlaughterAmount implements Runnable {
 		long beforeTimStamp = 0;
 		int teamid = 0;
 		long oneminute = 60000L;
+		
 		ArrayList<String> records = new ArrayList<>();
 		ArrayList<SlaughterAmountModel> slaughterArr = AddValuesToDB.getFillSlaughterAmountTable(dbSinCon);
-		for (int i = 0; i < slaughterArr.size(); i++) {
-			if( slaughterArr.get(i).getStarttimestamp() != beforeTimStamp){
-				timestamp = slaughterArr.get(i).getStarttimestamp();
-				beforeTimStamp = slaughterArr.get(i).getStarttimestamp();
-			}
-			for(int j = 0; j < slaughterArr.get(i).getNumOfMinBatch(); j++){
-				if(timestamp > slaughterArr.get(i).getEndtimestamp()){
-					teamid = slaughterArr.get(i).getTeamdaytimetableid();
-				}else{
-					teamid =  slaughterArr.get(i).getTeamnighttimetableid();
+		for (int k = 0; k <= 1; k++) {
+			for (int i = (slaughterArr.size()/3)*k; i < (slaughterArr.size()/3)*k+slaughterArr.size()/3; i++) {
+				if( slaughterArr.get(i).getStarttimestamp() != beforeTimStamp){
+					timestamp = slaughterArr.get(i).getStarttimestamp();
+					beforeTimStamp = slaughterArr.get(i).getStarttimestamp();
 				}
-				timestamp += oneminute;
-				tmpString += System.lineSeparator() + "INSERT INTO slaughteramount (value, batchid, satimestamp, teamtimetableid) VALUES (" + (slaughterArr.get(i).getOrganic() ? 88 : 212) + ", " + slaughterArr.get(i).getId() + ", " + timestamp + ", " + teamid + ");";
-				records.add(tmpString);
-				System.out.println("SA: " + i +" " + j + " timestamp: " + timestamp);
-				tmpString = "";
+				for(int j = 0; j <= slaughterArr.get(i).getNumOfMinBatch()/(k==0 ? 1 : 2); j++){
+					if(timestamp > slaughterArr.get(i).getEndtimestamp()){
+						teamid = slaughterArr.get(i).getTeamdaytimetableid();
+					}else{
+						teamid =  slaughterArr.get(i).getTeamnighttimetableid();
+					}
+					timestamp += oneminute;
+					tmpString += System.lineSeparator() + "INSERT INTO slaughteramount (value, batchid, satimestamp, teamtimetableid) VALUES (" + (slaughterArr.get(i).getOrganic() ? 88 : 212) + ", " + slaughterArr.get(i).getId() + ", " + timestamp + ", " + teamid + ");";
+					records.add(tmpString);
+					System.out.println("SA: " + i +" " + j + " timestamp: " + timestamp);
+					tmpString = "";
+				}
 			}
 		}
+		
 		Main.writeToFile(records, "Slaughter");
 		System.out.println("done!");
 	}
